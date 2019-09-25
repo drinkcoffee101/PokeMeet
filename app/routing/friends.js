e4e1740e8cdb8b14e9703babdb06fa74a5891865
drinkcoffee <dcoffie92@gmail.com>
2019-09-24 18:30:22 -0700

    notes

--- app/routing/friends.js
+++ app/routing/friends.js
@@ -2,43 +2,39 @@
 
 
 
-// const { app, connection } = require("../../server.js");
+// // const { app, connection } = require("../../server.js");
 
-// module.exports = (connection) => {}
+// // module.exports = (connection) => {}
 
-let findMatch = (userScore, cb) => {
-  connection.query("SELECT * FROM trainers ORDER BY id", (err, data) => {
-    if (err) throw err;
+// let findMatch = (userScore, cb) => {
+//   connection.query("SELECT * FROM trainers ORDER BY id", (err, data) => {
+//     if (err) throw err;
 
-    // console.log(data)
-    var match = {}
-    //1st match will always be 'a match' if the difference is below 100
-    var matchTotal = 100;
-    data.forEach(user => {
-      //calc the attr total of each past user and compare to the new user
-      var tempMtachTotal = Math.abs(formatScore(user.attr) - userScore)
-      if (tempMtachTotal < matchTotal) {
-        matchTotal = tempMtachTotal;
-        match = user;
-      }
-    });
-    cb(match);
-  });
-}
+//     // console.log(data)
+//     var match = {}
+//     //1st match will always be 'a match' if the difference is below 100
+//     var matchTotal = 100;
+//     data.forEach(user => {
+//       //calc the attr total of each past user and compare to the new user
+//       var tempMtachTotal = Math.abs(formatScore(user.attr) - userScore)
+//       if (tempMtachTotal < matchTotal) {
+//         matchTotal = tempMtachTotal;
+//         match = user;
+//       }
+//     });
+//     cb(match);
+//   });
+// }
 
-let formatScore = (str) => {
-  var userScore = str.split(',').map(Number);
-  var total = userScore.reduce((acc, curr) => acc + curr, 0);
-  return total;
-}
-
-
-// export {findMatch, formatScore};
-module.exports = { findMatch, formatScore };
+// let formatScore = (str) => {
+//   var userScore = str.split(',').map(Number);
+//   var total = userScore.reduce((acc, curr) => acc + curr, 0);
+//   return total;
+// }
 
 
-
-
+// // export {findMatch, formatScore};
+// module.exports = { findMatch, formatScore };
 
 
 
