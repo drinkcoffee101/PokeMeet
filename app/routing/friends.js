146884cd221d0871435872d5636008adfecff5ac
drinkcoffee <dcoffie92@gmail.com>
2019-10-04 21:57:56 -0700

    security

--- app/routing/friends.js
+++ app/routing/friends.js
@@ -38,6 +38,25 @@
 
 
 
+let arr1 = [['A1', '0.959', 'Plate 1'], ['A2', '1.022', 'Plate 1'], ['A3', '0.680', 'Plate 1'], ['A1', '9999', 'Plate 2'], ['A2', '99992', 'Plate 2'], ['A3', '000', 'Plate 2']]
+
+let arr2 = [['Plate 1', 'A1', 'pA08164', 113110.0], ['Plate 1', 'A2', 'pA08164', 113110.0], ['Plate 1', 'A3', 'pA08164', 113110.0], ['Plate 2', 'A1', 'pA08164', 90909], ['Plate 2', 'A2', 'pA08164', 5555510.0], ['Plate 2', 'A3', 'pA08164', 6776.0]]
+
+let matchingVlaues = (arr1, arr2) => {
+    var tempArr = []
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr1[i].includes(arr2[j][0]) && arr1[i].includes(arr2[j][1])) {
+                tempArr = arr2[j].slice(2);
+                 arr1[i] = arr1[i].concat(tempArr);
+            }
+        }
+    }
+}
+
+matchingVlaues(arr1,arr2);
+
+console.log(arr1);
 
 
 
